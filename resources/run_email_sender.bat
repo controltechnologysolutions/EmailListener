b7382a6b8bd8f31d13483f3e05a431cff9dca8db
lhrolim <luizhenrique.rolim@gmail.com>
2016-12-08 13:36:51 -0200

    update props

--- resources/run_email_sender.bat
+++ resources/run_email_sender.bat
@@ -2,6 +2,6 @@
 
 SETLOCAL
 
-set CS_CLASSPATH=lib\mail.jar;lib\Opta.jar;lib\commons-io-2.4.jar;lib\commons-codec-1.4.jar;classes
+set CS_CLASSPATH=lib\mail.jar;lib\Opta.jar;lib\commons-io-2.4.jar;lib\commons-codec-1.4.jar;.
 
-java -classpath %CS_CLASSPATH% com.cts.emailsender.EmailSender
+java -cp %CS_CLASSPATH% com.cts.emailsender.EmailSender
